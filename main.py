19b40f27a9dd6b8dd5c856c8c4ab7801435a2610
pacificbelt30 <pacificbelt0201@outlook.jp>
2021-01-07 17:30:52 +0900

    first

---
+++ main.py
@@ -0,0 +1,93 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+# import sip
+
+
+class MainWindow(QWidget):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.row_limit_words = 30
+        self.font_size = 15
+        self.line_count = 0
+        self.line_word_count = [0]
+
+        # self.test = QCheckBox('test', self)
+        self.btn = QPushButton("BUTTON", self)
+        self.btn.clicked.connect(self.print_praintext)
+        self.textbox = QTextEdit()
+        font = QFont()
+        font.setPointSize(self.font_size)
+        font_met = QFontMetrics(font)
+        self.textbox.setFont(font)
+        # self.textbox = QPlainTextEdit()
+        # self.textbox.setLineWrapMode(QTextEdit.FixedColumnWidth)
+        self.textbox.setLineWrapMode(3)  # 文字数での折返し
+        self.textbox.setLineWrapColumnOrWidth(self.row_limit_words)
+        self.textbox.setFixedHeight(font_met.height()*30)
+        self.textbox.setViewportMargins(font_met.width('x')*3, 0, 0, 0)
+        # self.textbox.setFontPointSize(15.0)
+        # self.textbox.setFontUnderline(True)
+        # self.textbox.setMaximumBlockCount(10)
+        # self.addWidget(self.textbox)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.btn)
+        layout.addWidget(self.textbox)
+        self.setGeometry(300, 50, 650, 550)
+        self.setWindowTitle('QCheckBox')
+
+    def print_praintext(self):
+        print(self.textbox.toPlainText())
+        s = self.textbox.toPlainText()
+        print(s.split('\n'))
+        # print(self.textbox.blockCount())
+        # self.sliceMojisu(self.textbox.toPlainText().split('\n'))
+        print(self.get_line_count())
+        print(self.get_line_word_count())
+
+    def get_line_count(self):
+        # count = 0
+        text = self.textbox.toPlainText().split('\n')
+        count = len(text)
+        print(len(text))
+        for s in text:
+            if len(s) % 30 == 0 and len(s) != 0:
+                count = count - 1
+            count = count + (len(s) // 30)
+            # print(len(s))
+            # print(s)
+        self.line_count = count
+        return count
+
+    def get_line_word_count(self):
+        count = []
+        for s in self.textbox.toPlainText().split('\n'):
+            count.append(len(s))
+        self.line_word_count = count
+        return count
+
+class repoWindow(QWidget):
+    def __init__(self, text, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.report_area = QTextEdit()
+        self.a4word = 30
+        self.a4row = 20
+        self.text = text
+        self.report_area.setText(text)
+
+    def cut_a4size(self):
+        self.report_area
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
