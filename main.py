d20632062b4750883f6f5e5bd777c518e1c886dd
pacificbelt30 <pacificbelt0201@outlook.jp>
2021-01-07 20:55:08 +0900

    現在上限を超えて入力を受け付けないようにしようとしているところであります

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class ReportArea(QTextEdit):
         self.row = 20
         self.column = 30
         self.font_size = 15
+        self.line_count = 0
+        self.line_word_count = 0
         font = QFont()
         font.setPointSize(self.font_size)
         font_met = QFontMetrics(font)
@@ -29,6 +31,63 @@ class ReportArea(QTextEdit):
         self.setLineWrapColumnOrWidth(self.column)
         self.setFixedHeight(font_met.height()*self.row)
         self.setViewportMargins(font_met.width('x')*3, 0, 0, 0)
+        # self.tmp = QTextBlock()
+
+    # DEBUG用
+    def print_plaintext(self):
+        print(self.textbox.toPlainText())
+        s = self.textbox.toPlainText()
+        print(s.split('\n'))
+        print(self.get_line_count())
+        print(self.get_line_word_count())
+
+    # 全体の行数を取得 self.columnを上回り折り返された部分は新たな行としてカウントする
+    def get_line_count(self):
+        # count = 0
+        text = self.textbox.toPlainText().split('\n')
+        count = len(text)
+        print(len(text))
+        for s in text:
+            if len(s) % 30 == 0 and len(s) != 0:
+                count = count - 1
+            count = count + (len(s) // 30)
+            # print(len(s))
+            # print(s)
+        self.line_count = count
+        return count
+
+    # 各行の文字数
+    def get_line_word_count(self):
+        count = []
+        for s in self.textbox.toPlainText().split('\n'):
+            count.append(len(s))
+        self.line_word_count = count
+        return count
+
+    # self.rowの制限に引っかかる限界の文字数を取得
+    def limit_word_count(self):
+        count = 0
+        mojisu = 0
+        text = self.textbox.toPlainText().split('\n')
+        # count = len(text)
+        print(len(text))
+        for s in text:
+            if len(s) == 0:
+                count = count + 1
+            elif len(s) % 30 != 0:
+                count = count + 1
+            count = count + (len(s) // 30)
+            if count > self.row:
+                return
+            # print(len(s))
+            # print(s)
+        self.line_count = count
+        return count
+
+    def txt_input_limit(self):
+        if self.get_line_count() > self.row:
+            tmp = self.toPlainText()
+            tmp = tmp[:]
 
 
 class MainWindow(QWidget):
@@ -47,6 +106,7 @@ class MainWindow(QWidget):
         layout.addWidget(self.btn)
         layout.addWidget(self.textbox)
         self.setGeometry(300, 50, 650, 550)
+        self.setFixedSize(650, 550) # サイズ変更不可能にした
         self.setWindowTitle('QCheckBox')
 
     def print_plaintext(self):
