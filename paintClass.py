53568a6637022ee9d0b565717ee76d496fda5fb6
pacificbelt30 <pacificbelt0201@outlook.jp>
2021-01-11 01:41:48 +0900

    slot

--- paintClass.py
+++ paintClass.py
@@ -1,15 +1,18 @@
 import sys
+import json
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QGraphicsView
 from PyQt5.QtGui import QPainter, QImage, QPen, qRgb, QPixmap
-from PyQt5.QtCore import Qt, QPoint, QRect, QSize
+from PyQt5.QtCore import Qt, QPoint, QRect, QSize, pyqtSignal, pyqtSlot
 import matplotlib.pyplot as plt
 
 class Canvas(QWidget):
+  moji_fin=pyqtSignal()
   def __init__(self):
     super().__init__()
-
     self.countx = list()
     self.county = list()
+    self.kakusu = 99
+    self.text = ""
     self.count = 0
     self.myPenWidth = 2
     self.myPenColor = Qt.black
@@ -46,17 +49,35 @@ class Canvas(QWidget):
       self.check = False
 
       self.count = self.count+1
-      if self.count == 3:
-          print('count==2')
-          plt.xlim(0,500)
-          plt.ylim(500,0)
-          plt.plot(self.countx[0],self.county[0]);
-          plt.plot(self.countx[1],self.county[1]);
-          plt.plot(self.countx[2],self.county[2]);
-          print('一画目'+str(len(self.countx[0]))+' '+str(len(self.county[0])))
-          print('二画目'+str(len(self.countx[1]))+' '+str(len(self.county[1])))
-          print('三画目'+str(len(self.countx[2]))+' '+str(len(self.county[2])))
-          plt.show()
+      if self.count == self.kakusu:
+          self.moji_fin.emit()
+          #print('count==3')
+          #plt.xlim(0,500)
+          #plt.ylim(500,0)
+          #plt.plot(self.countx[0],self.county[0])
+          #plt.plot(self.countx[1],self.county[1])
+          #plt.plot(self.countx[2],self.county[2])
+          #print('一画目'+str(len(self.countx[0]))+' '+str(len(self.county[0])))
+          #print('二画目'+str(len(self.countx[1]))+' '+str(len(self.county[1])))
+          #print('三画目'+str(len(self.countx[2]))+' '+str(len(self.county[2])))
+          #tmp = {self.text:{"data":{"x":self.countx,"y":self.county}}}
+          #json.dumps(tmp)
+          #with open('test.json', 'w') as f:
+              #json.dump(tmp, f, indent=4)
+          #self.clear()
+          #plt.show()
+
+  def clear(self):
+    self.countx = list()
+    self.county = list()
+    self.count = 0
+    painter = QPainter(self.image)
+    painter.setPen(
+      QPen(Qt.white)
+    )
+    rect = QRect(0, 0, 300, 300)
+    painter.fillRect(rect, Qt.white)
+    self.update()
 
 
   def drawLine(self, endPos):
@@ -106,6 +127,7 @@ class MainWindow(QWidget):
         super().__init__()
         self.title="test"
         self.text = "あ"
+        self.kakusu = 3
         self.width = 400
         self.height = 400
         self.initUI()
@@ -120,12 +142,36 @@ class MainWindow(QWidget):
         self.canvas = Canvas()
         self.button = QPushButton()
         self.label = QLabel("「"+self.text+"」"+"を描いてください")
+        self.canvas.kakusu = self.kakusu
+        self.canvas.text = self.text
+        self.canvas.moji_fin.connect(self.save_data)
         self.layout = QVBoxLayout()
         #self.layout = QHBoxLayout()
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.canvas)
         self.layout.addWidget(self.button)
         self.setLayout(self.layout)
+
+    # 描画データを保存し，まっさらにするスロット
+    @pyqtSlot()
+    def save_data(self):
+        print('count==3')
+        plt.xlim(0,500)
+        plt.ylim(500,0)
+        plt.plot(self.canvas.countx[0],self.canvas.county[0])
+        plt.plot(self.canvas.countx[1],self.canvas.county[1])
+        plt.plot(self.canvas.countx[2],self.canvas.county[2])
+        print('一画目'+str(len(self.canvas.countx[0]))+' '+str(len(self.canvas.county[0])))
+        print('二画目'+str(len(self.canvas.countx[1]))+' '+str(len(self.canvas.county[1])))
+        print('三画目'+str(len(self.canvas.countx[2]))+' '+str(len(self.canvas.county[2])))
+        tmp = {self.text:{"data":{"x":self.canvas.countx,"y":self.canvas.county}}}
+        json.dumps(tmp)
+        with open('test.json', 'w') as f:
+            json.dump(tmp, f, indent=4)
+        self.canvas.clear()
+        plt.show()
+
+
 
 if __name__ == '__main__':
   # 動け～
---
+++ svgout.py
@@ -0,0 +1,39 @@
+#! usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, absolute_import
+import sys
+#from PySide.QtCore import *
+#from PySide.QtGui import *
+#from PySide.QtSvg import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtSvg import *
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QGraphicsView
+
+x = [[22, 23, 26, 28, 32, 36, 39, 42, 50, 55, 61, 67, 70, 72, 73, 74], [46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 47, 47, 47, 48, 49, 49, 50, 51, 51, 52, 53, 56, 57, 58, 58, 59, 62, 63, 64], [64, 64, 64, 64, 64, 63, 62, 61, 59, 58, 57, 56, 55, 54, 52, 51, 49, 46, 45, 43, 41, 40, 39, 38, 37, 37, 37, 37, 37, 37, 37, 37, 37, 38, 40, 41, 42, 43, 45, 46, 50, 52, 53, 55, 57, 58, 61, 64, 67, 69, 71, 73, 73, 74, 74, 74, 75, 75, 76, 76, 76, 76, 76]]
+y = [[53, 53, 53, 53, 53, 53, 52, 51, 49, 47, 45, 45, 45, 45, 45, 45], [43, 45, 46, 47, 50, 51, 53, 58, 61, 62, 64, 65, 67, 68, 69, 70, 71, 73, 74, 75, 75, 76, 77, 78, 80, 81, 81, 82, 82, 83, 83, 83], [73, 74, 77, 78, 79, 79, 79, 79, 79, 79, 79, 79, 79, 79, 78, 78, 78, 78, 78, 78, 78, 78, 78, 76, 74, 73, 72, 71, 70, 68, 67, 66, 64, 63, 63, 62, 62, 62, 62, 62, 62, 62, 62, 62, 62, 62, 63, 64, 65, 67, 69, 71, 72, 73, 74, 75, 76, 78, 79, 80, 81, 82, 83]]
+app = QApplication(sys.argv)
+svg_gen = QSvgGenerator()
+svg_gen.setFileName("test.svg")
+svg_gen.setSize(QSize(200, 200))
+svg_gen.setViewBox(QRect(0, 0, 200, 200))
+svg_gen.setTitle("echo あ")
+svg_gen.setDescription("from pyqt QtSvg test.")
+
+
+painter = QPainter()
+painter.begin(svg_gen)
+painter.setPen(Qt.black)
+
+
+rect = QRect(0, 0, 200, 200)
+painter.fillRect(rect, Qt.white)
+
+#painter.setPen(Qt.blue)
+#painter.setFont(QFont("Arial", 30))
+#painter.drawText(rect, Qt.AlignCenter, "Hello SVG")
+for i in range(len(x)):
+    for j in range(len(x[i])-1):
+        painter.drawLine(x[i][j],y[i][j],x[i][j+1],y[i][j+1])
+
+painter.end()
