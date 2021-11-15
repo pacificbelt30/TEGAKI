8a1cf4109e8f6927e62c8dab4501e4269d65d45d
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-11-16 01:15:09 +0900

    文字の太さを調整

--- svgout.py
+++ svgout.py
@@ -17,13 +17,14 @@ import math
 # ヘッダー 42.5mm + 1mm, フッター 15mm + 0.5mm
 #A罫 左 7mm, 右 7mm, 補助線7mmx28
 #B罫 左 6mm, 右 6mm, 補助線6mmx35
+# textに平文を入れてself.gen()
 class A4_svgenerator:
     def __init__(self):
         self._svg: QSvgGenerator = QSvgGenerator()
         #self._title: str = "test.svg"
         self._title: str = "test"
-        self._hol:int = 210*100
-        self._ver:int = round(self.hol*math.sqrt(2))
+        self._hol:int = 210*100 # 横ピクセル
+        self._ver:int = round(self.hol*math.sqrt(2)) # 縦ピクセル
         self._row: int = 34  # 行
         self._column: int = 28  # 列
         self._pixel: int = int(self.hol/30)  # 一文字分のピクセル
@@ -139,8 +140,11 @@ class A4_svgenerator:
             self.setSVGConfig(file[0]+"_"+str(self.page)+".svg")
             print("page:"+str(self.page))
             painter = QPainter()
+            pen = QPen()
+            pen.setWidth(5)
+            pen.setColor(Qt.black)
             painter.begin(self.svg)
-            painter.setPen(Qt.black)
+            painter.setPen(pen)
             #rect = QRect(0, 0, self.column * self.pixel + (self.left_margin + self.right_margin),
                                       #self.row * (self.line_margin + self.pixel) + (
                                                   #self.head_margin + self.bottom_margin))
@@ -200,3 +204,4 @@ class A4_svgenerator:
 
     def trans_han_to_zen(self,text:str):
         return text.translate(str.maketrans({chr(0xFF01 + i): chr(0x21 + i) for i in range(94)}))
+
