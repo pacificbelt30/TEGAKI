4ee6e30209f78cb690d05d2c39dc58dd78c9380a
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2021-11-29 02:04:44 +0900

    a4 size svg

--- svgout.py
+++ svgout.py
@@ -23,11 +23,11 @@ class A4_svgenerator:
         self._svg: QSvgGenerator = QSvgGenerator()
         #self._title: str = "test.svg"
         self._title: str = "test"
-        self._hol:int = 210*10 # 横ピクセル
+        self._hol:int = 210*100 # 横ピクセル
         self._ver:int = round(self.hol*math.sqrt(2)) # 縦ピクセル
         self._row: int = 34  # 行
         self._column: int = 28  # 列
-        self._pixel: int = int(self.hol*10/30)  # 一文字分のピクセル
+        self._pixel: int = int(self.hol/30)  # 一文字分のピクセル
         self._head_margin: int = round(43.5/297*self.ver)
         self._bottom_margin: int = self.ver-self.head_margin+self.row*self.pixel
         self._right_margin: int = int(self.hol/30)
@@ -45,8 +45,8 @@ class A4_svgenerator:
         # self.svg.setViewBox(QRect(0, 0,self.row*(self.line_margin+self.pixel)+(self.head_margin+self.bottom_margin),self.column*self.pixel+(self.left_margin+self.right_margin)))
         #self.svg.setSize(QSize(self.column*self.pixel+(self.left_margin+self.right_margin),self.row*(self.line_margin+self.pixel)+(self.head_margin+self.bottom_margin)))
         #self.svg.setViewBox(QRect(0,0,self.column*self.pixel+(self.left_margin+self.right_margin),self.row*(self.line_margin+self.pixel)+(self.head_margin+self.bottom_margin)))
-        self.svg.setSize(QSize(self.hol,self.ver))
-        self.svg.setViewBox(QRect(0,0,self.hol*10,self.ver*10))
+        self.svg.setSize(QPageSize(QPageSize.A4).sizePoints())
+        self.svg.setViewBox(QRect(0,0,self.hol,self.ver))
         # self.svg.setSize(QSize(500,500))
         # self.svg.setViewBox(QRect(0, 0,500,500))
         self.svg.setTitle(self.title)
@@ -148,7 +148,7 @@ class A4_svgenerator:
             #rect = QRect(0, 0, self.column * self.pixel + (self.left_margin + self.right_margin),
                                       #self.row * (self.line_margin + self.pixel) + (
                                                   #self.head_margin + self.bottom_margin))
-            rect = QRect(0, 0,self.hol*10,self.ver*10)
+            rect = QRect(0, 0,self.hol,self.ver)
             painter.fillRect(rect, Qt.white)
             i: int = 0
             while True:
@@ -195,9 +195,10 @@ class A4_svgenerator:
         # self.svg.setViewBox(QRect(0, 0,self.row*(self.line_margin+self.pixel)+(self.head_margin+self.bottom_margin),self.column*self.pixel+(self.left_margin+self.right_margin)))
         #self.svg.setSize(QSize(self.column*self.pixel+(self.left_margin+self.right_margin),self.row*(self.line_margin+self.pixel)+(self.head_margin+self.bottom_margin)))
         #self.svg.setViewBox(QRect(0,0,self.column*self.pixel+(self.left_margin+self.right_margin),self.row*(self.line_margin+self.pixel)+(self.head_margin+self.bottom_margin)))
-        self.svg.setSize(QSize(self.hol,self.ver))
+        #self.svg.setSize(QSize(self.hol,self.ver))
+        self.svg.setSize(QPageSize(QPageSize.A4).sizePoints())
         print(self.hol,self.ver)
-        self.svg.setViewBox(QRect(0,0,self.hol*10,self.ver*10))
+        self.svg.setViewBox(QRect(0,0,self.hol,self.ver))
         # self.svg.setSize(QSize(500,500))
         # self.svg.setViewBox(QRect(0, 0,500,500))
         self.svg.setTitle(file)
