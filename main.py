1925de98323f247c827b3eb93b95dcfc37a4a8eb
pacificbelt30 <57101176+pacificbelt30@users.noreply.github.com>
2022-07-20 20:36:10 +0900

    I already don't use pycharm

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import sys
 import os
 from PySide2.QtCore import *
